1dc0e348333790849c1207504cd358ef45864124
alexag <alxick@gmail.com>
2018-11-05 12:06:12 +0300

    Updates

---
+++ precache-manifest.e00d61e240584a46088a48a518fa44e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "437b7a0d96016e2eadd0",
+    "url": "/weather-app/static/css/main.e85426a5.chunk.css"
+  },
+  {
+    "revision": "437b7a0d96016e2eadd0",
+    "url": "/weather-app/static/js/main.437b7a0d.chunk.js"
+  },
+  {
+    "revision": "97a3a0153407c1e96a74",
+    "url": "/weather-app/static/js/1.97a3a015.chunk.js"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "11ab37ad54488ee7f318f929bc406714",
+    "url": "/weather-app/static/media/update.11ab37ad.svg"
+  },
+  {
+    "revision": "35a6ccb5ed555dd6c31878ae02c9fd0d",
+    "url": "/weather-app/static/media/error.35a6ccb5.svg"
+  },
+  {
+    "revision": "b2138751ec3c40bb5c24fcea41aaff9d",
+    "url": "/weather-app/static/media/error_hover.b2138751.svg"
+  },
+  {
+    "revision": "ee70151a3df292330270add5b30e9977",
+    "url": "/weather-app/index.html"
+  }
+];
